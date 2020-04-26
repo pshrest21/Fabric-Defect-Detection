f000a8a211a94b73fc04925377cdb2895404b6f5
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2020-04-26 12:23:11 -0500

    Defect Detectiong for all 8 fabrics completed successfully

--- H-Image.py
+++ H-Image.py
@@ -1,14 +1,7 @@
 import cv2
 import numpy as np
+from PIL import Image, ImageFilter 
 
-fabric1 = cv2.imread('1.jpg',0)
-fabric2 = cv2.imread('2.jpg',0)
-fabric3 = cv2.imread('3.jpg',0)
-fabric4 = cv2.imread('4.jpg',0)
-fabric5 = cv2.imread('5.jpg',0)
-fabric6 = cv2.imread('6.jpg',0)
-fabric7 = cv2.imread('7.jpg',0)
-fabric8 = cv2.imread('8.jpg',0)
 
 def dft(img):
     f = np.fft.fft2(img)  
@@ -28,20 +21,6 @@ def high_pass_filter(img,n):
     img_back = np.fft.ifft2(f_ishift)
     img_back = np.abs(img_back)
     return img_back
-
-def low_pass_filter(img,n):
-    fshift, ms = dft(img)
-    rows, cols = img.shape
-    crow,ccol = rows/2 , cols/2
-    
-    mask = np.zeros([rows, cols])
-    mask[int(crow)-n:int(crow)+n, int(ccol)-n:int(ccol)+n] = 1
-    
-    fshift = fshift*mask    
-    f_ishift = np.fft.ifftshift(fshift)
-    img_back = np.fft.ifft2(f_ishift)
-    img_back = np.abs(img_back)
-    return img_back 
 
 def HIPI(img):
     new_img = np.zeros((512, 512), dtype = np.uint8)
@@ -63,14 +42,6 @@ def HIPI(img):
         
     return new_img
 
-def gaussian_filter(img, figure_size):
-    new_image = cv2.blur(img, (figure_size, figure_size))
-    return np.array(new_image) 
-
-def canny_edge(img, x,y, n):
-    img = np.uint8(img)
-    new_image = cv2.Canny(img, x, y, apertureSize=n)
-    return np.array(new_image)
 
 def erosion(img, kernel, n):
     #erosion using opencv inbuilt function cv2.erode()
@@ -83,6 +54,8 @@ def dialation(img, kernel, n):
 
 
 
+
+
 #--------------------Computation Part------------
 #kernels to use
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
@@ -90,7 +63,10 @@ kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
 kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT,(7,2))
 kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT,(9,1))
 
+
+
 #----------------------Fabric1--------------------------
+fabric1 = cv2.imread('1.jpg', 0)
 fabric1 = cv2.equalizeHist(fabric1) #equalize the image
 ret, new_img = cv2.threshold(fabric1, 215,255,cv2.THRESH_BINARY) #convert to binary image
 new_img = erosion(new_img, kernel2, 1) #removes excess noise from the image
@@ -98,133 +74,181 @@ new_img = cv2.medianBlur(new_img, 3) #Removes the salt noise
 new_img = dialation(new_img, kernel3, 5) #dilates the image 5 times
 cv2.imwrite('detect_1.jpg', new_img)
 
-#---old code for fabric1---
-#fabric1 = high_pass_filter(fabric1, 50) #Use high pass filter to filter low frequency component
-#new_img = HIPI(fabric1) #get the H-Image
-#ret, new_img = cv2.threshold(new_img,20,255,cv2.THRESH_BINARY_INV) #convert to binary image
 
-#new_img = cv2.medianBlur(new_img, 5) #Removes the salt noise
-#cv2.imshow('detect_1.jpg', new_img)
 
 
 
 #----------------------Fabric2--------------------------
-fabric2 = cv2.equalizeHist(fabric2) #equalize the image
-ret, new_img = cv2.threshold(fabric2,20,255,cv2.THRESH_BINARY) #convert to binary image
-new_img = erosion(new_img, kernel1, 5) #erodes the image with kernel1 5 times
-new_img = dialation(new_img, kernel1, 2) #dilates the image with kernel1 1 time
+fabric2 = Image.open('2.jpg', 'r')
+fabric2 = fabric2.filter(ImageFilter.MinFilter (size = 3))
+fabric2 = np.array(fabric2)
+new_img = cv2.cvtColor(fabric2, cv2.COLOR_BGR2GRAY)
+ret, new_img = cv2.threshold(new_img,190,255,cv2.THRESH_BINARY) #convert to binary image
+new_img = cv2.medianBlur(new_img, 11) #Removes the salt noise
+new_img = erosion(new_img, kernel1, 1)
 cv2.imwrite('detect_2.jpg', new_img)
 
 
 
-#----------------------Fabric4---------------------------
-fabric4 = cv2.equalizeHist(fabric4) #equalize the image
-ret, new_img = cv2.threshold(fabric4,0,150,cv2.THRESH_BINARY_INV) #convert to binary image
+
+
+#----------------------Fabric3--------------------------
+fabric3 = cv2.imread('3.jpg',0)
+ret, new_img = cv2.threshold(fabric3,150,255,cv2.THRESH_BINARY) #convert to binary image
 new_img = cv2.medianBlur(new_img, 3) #Removes the salt noise
-new_img = dialation(new_img, kernel1, 1)
+new_img = dialation(new_img, kernel1, 1) #Dilate the image
+new_img = erosion(new_img, kernel1, 1) #Erode the image
+new_img = cv2.medianBlur(new_img, 3) #Removes the salt noise
+cv2.imwrite('detect_3_1.jpg', new_img)
+
+
+
+#----------------------Fabric3 (Second Approach)--------------------------
+fabric3 = cv2.imread('3.jpg',0)
+fabric3 = high_pass_filter(fabric3, 50) #Use high pass filter to filter low frequency component
+new_img = HIPI(fabric3) #get the H-Image
+new_img = Image.fromarray(new_img)
+new_img = new_img.filter(ImageFilter.MinFilter(size = 3)) #apply the min filter
+new_img = np.array(new_img)
+ret, new_img = cv2.threshold(new_img,21,255,cv2.THRESH_BINARY) #convert to binary image
+new_img = cv2.medianBlur(new_img, 7) #Removes the salt noise
+cv2.imwrite('detect_3_2.jpg', new_img)
+
+
+
+
+
+
+#----------------------Fabric4---------------------------
+fabric4 = Image.open('4.jpg', 'r')
+fabric4 = fabric4.filter(ImageFilter.MinFilter (size = 3))
+fabric4 = np.array(fabric4)
+new_img = cv2.cvtColor(fabric4, cv2.COLOR_BGR2GRAY)
+ret, new_img = cv2.threshold(new_img,120,255,cv2.THRESH_BINARY_INV) #convert to binary image
+new_img = cv2.medianBlur(new_img, 7) #Removes the salt noise
 cv2.imwrite('detect_4.jpg', new_img)
 
-#---old code for fabric4 ----
-#fabric4 = high_pass_filter(fabric4, 50) #Use high pass filter to filter low frequency component
-#new_img = HIPI(fabric4) #get the H-Image
-#ret, new_img = cv2.threshold(new_img,20,255,cv2.THRESH_BINARY) #convert to binary image
 
-#new_img = cv2.medianBlur(new_img, 5) #Removes the salt noise
-#new_img = erosion(new_img, kernel2, 1)
-#new_img = dialation(new_img, kernel3, 1)
-#cv2.imshow('detect_4.jpg', new_img)
+
+
 
 
 
 #----------------------Fabric5---------------------------
-fabric5 = cv2.equalizeHist(fabric5) #equalize the image
-ret, new_img = cv2.threshold(fabric5,0,255,cv2.THRESH_BINARY_INV) #convert to binary image
-new_img = cv2.medianBlur(new_img, 3) #Removes the salt noise
-new_img = dialation(new_img, kernel3, 4) #dilates the image and joins the white points
+fabric5 = Image.open('5.jpg', 'r')
+fabric5 = fabric5.filter(ImageFilter.MinFilter (size = 3))
+fabric5 = np.array(fabric5)
+new_img = cv2.cvtColor(fabric5, cv2.COLOR_BGR2GRAY)
+ret, new_img = cv2.threshold(new_img,120,255,cv2.THRESH_BINARY_INV) #convert to binary image
+new_img = cv2.medianBlur(new_img, 5) #Removes the salt noise
 cv2.imwrite('detect_5.jpg', new_img)
 
 
+
+
+
+
 #----------------------Fabric6---------------------------
-fabric6 = cv2.equalizeHist(fabric6) #equalize the image
-ret, new_img = cv2.threshold(fabric6,200,255,cv2.THRESH_BINARY) #convert to binary image
-new_img = cv2.medianBlur(new_img, 11) #Removes the salt noise
-cv2.imwrite('detect_6.jpg',new_img)
+fabric6 = Image.open('6.jpg', 'r')
+fabric6 = fabric6.filter(ImageFilter.MinFilter(size = 3))
+fabric6 = np.array(fabric6)
+new_img = cv2.cvtColor(fabric6, cv2.COLOR_BGR2GRAY)
+ret, new_img = cv2.threshold(new_img,100,255,cv2.THRESH_BINARY) #convert to binary image
+new_img = cv2.medianBlur(new_img, 5) #Removes the salt noise
+cv2.imwrite('detect_6.jpg', new_img)
+
+
+
+
+
+
+#----------------------Fabric7---------------------------
+fabric7 = cv2.imread('7.jpg',0)
+fabric7 = high_pass_filter(fabric7, 50)
+new_img = HIPI(fabric7)
+new_img = Image.fromarray(new_img)
+new_img = new_img.filter(ImageFilter.MinFilter (size = 3))
+new_img = np.array(new_img)
+ret, new_img = cv2.threshold(new_img,10,255,cv2.THRESH_BINARY)
+new_img  = cv2.medianBlur(new_img, 11)
+cv2.imwrite('detect_7.jpg',new_img)
+
+
+
 
 
 
 #----------------------Fabric8---------------------------
-fabric8 = cv2.equalizeHist(fabric8)
-fabric8 = high_pass_filter(fabric8, 50) #Use high pass filter to filter low frequency component
-new_img = HIPI(fabric8) #get the H-Image
-ret, new_img = cv2.threshold(new_img,20,255,cv2.THRESH_BINARY) #convert to binary image
+fabric8 = Image.open('8.jpg', 'r')
+fabric8 = fabric8.filter(ImageFilter.MinFilter (size = 3))
+fabric8 = np.array(fabric8)
+new_img = cv2.cvtColor(fabric8, cv2.COLOR_BGR2GRAY)
+ret, new_img = cv2.threshold(new_img,85,255,cv2.THRESH_BINARY_INV) #convert to binary image
 new_img = cv2.medianBlur(new_img, 5) #Removes the salt noise
-new_img = erosion(new_img, kernel, 1)
-new_img = dialation(new_img, kernel1, 1)
-cv2.imshow('detect_8.jpg', new_img)
+cv2.imwrite('detect_8.jpg', new_img)
 
 
-
-
-
-
-
-
-#----------------------Fabric3---------------------------
-
-
-#----------------------Fabric7---------------------------
-fabric7 = cv2.equalizeHist(fabric7) #equalize the image
-ret, new_img = cv2.threshold(fabric7,200,255,cv2.THRESH_BINARY) #convert to binary image
-
-#cv2.imshow('detect_7.jpg',new_img)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-fabric1 = high_pass_filter(fabric1, 100)
-new_img = HIPI(fabric1)
-ret, new_img = cv2.threshold(new_img,10,255,cv2.THRESH_BINARY_INV)
-
-new_img = erosion(new_img, kernel1, 3)
-new_img = dialation(new_img, kernel1, 3)
-
-cv2.imshow('defect_1.jpg', new_img)
-
-for i in range(4, 9):
-    fabric = cv2.imread(str(i)+'.jpg',0)
-    #replicate = cv2.copyMakeBorder(fabric1,1,1,1,1,cv2.BORDER_REPLICATE)
-    
-    replicate = high_pass_filter(fabric, 50)
-    
-    new_img = HIPI(replicate)
-    ret, new_img = cv2.threshold(new_img,10,255,cv2.THRESH_BINARY)
-    
-    new_img = erosion(new_img, kernel1, 3)
-    new_img = erosion(new_img, kernel, 1)
-    new_img = dialation(new_img, kernel2, 1) 
-    new_img = dialation(new_img, kernel3, 1)
-    
-    cv2.imwrite('detect_'+str(i)+'.jpg',new_img)
-'''
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
+
+'''
+new_ms = np.array(ms, dtype=np.uint8)
+ret, new_img = cv2.threshold(new_ms,190,255,cv2.THRESH_BINARY) #convert to binary image
+#new_img = cv2.medianBlur(new_img, 3) #Removes the salt noise
+
+new_img = erosion(new_img, kernel5, 1)
+new_img = dialation(new_img, kernel5, 5)
+
+#cv2.imshow('dft', new_img)
+
+
+lines = cv2.HoughLines(new_img,1,np.pi/180,200)
+new_ms = cv2.UMat(new_ms)
+
+for rho,theta in lines[0]:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv2.line(new_ms,(x1,y1),(x2,y2),(0,0,0),2)
+
+cv2.imshow('Magnitude Spectrum after adding line', new_ms)
+
+ms = ms.astype(np.uint8)
+new_ms = new_ms.get()
+
+new_ms = ms - new_ms
+
+f = np.fft.fft2(new_ms)  
+ft = np.fft.fftshift(f)
+new_ms = 20 * np.log(np.abs(ft))
+
+displayFourierImage(ms, new_ms, 'Defect area')
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
